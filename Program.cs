b2a5a3ea751d7b76139df3da981e7cc046537f71
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2022-02-23 14:13:43 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Password_Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
